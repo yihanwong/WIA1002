17882f87542cb2183b166cf2b1f651357fe5953f
flowongly <133207248+flowongly@users.noreply.github.com>
2023-05-14 22:45:20 +0800

    Update ImageProcessor.java

--- ImageProcessor.java
+++ ImageProcessor.java
@@ -3,12 +3,12 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class ImageProcessor {
+public class ImageProcessor2 {
 
     public static void main(String[] args) {
         // File paths for four images
         String[] imagePaths = {
-                "image 1.png",
+                "mage 1.png",
                 "image 2.png",
                 "image 3.png",
                 "image 4.png"
@@ -31,11 +31,28 @@ public class ImageProcessor {
             int height = image.getHeight();
             System.out.println("Image " + path + " dimensions: " + width + " x " + height);
 
-            // Iterate over each pixel and print its red component value
+            // Iterate over each pixel and print its converted red component value
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
                     int rgb = image.getRGB(x, y);
                     int red = (rgb >> 16) & 0xFF;
+                    switch (red) {
+                        case 0:
+                            red = 0;
+                            break;
+                        case 64:
+                            red = 1;
+                            break;
+                        case 128:
+                            red = 2;
+                            break;
+                        case 192:
+                            red = 3;
+                            break;
+                        default:
+                            // Do nothing
+                            break;
+                    }
                     System.out.print(red + " ");
                 }
                 System.out.println(); // Move to the next line after each row
